d96801cbb374f62310b8dec3cf86769f4c2c283d
4msdcn7zs9-lang <4msdcn7zs9@privaterelay.appleid.com>
2026-06-01 19:45:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
-print("Bot is running")
+import os
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command("chatid"))
+async def chatid(message: Message):
+    await message.answer(f"ID этой группы:\n{message.chat.id}")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
